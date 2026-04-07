ea7a11beae5b2dc71df1ba40921461c93f3189bf
afiqhaiqal-code <afiqhaiqal.yusri@gmail.com>
2026-04-07 12:16:02 +0800

    tailwind fix Part 6

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom"; // ✅ USE THIS
 import { router } from "./app/routes"; // adjust path if needed
 // Update the path below to match your actual CSS file location
-import "./styles/index.css";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <RouterProvider router={router} />
