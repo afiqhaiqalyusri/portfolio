562ba447fe9510cf7cd188be1df1bdff0d3e4945
afiqhaiqal-code <afiqhaiqal.yusri@gmail.com>
2026-04-07 12:11:05 +0800

    tailwind Part 5

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()], // ✅ THIS LINE IS CRITICAL
+  plugins: [react()],
   base: "/portfolio/",
 })
